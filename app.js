9ef58367166990f9e4612de57084e585a7410ea6
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-04-24 00:24:52 +0200

    [DONE]

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const postsRouter = require('./routers/posts'); // IMPORTA il file posts.js
 
-app.get('/',(req, res)=>{
-    res.send('Server Express Attivo!')
-});
+app.use(express.json()); // Serve per leggere i dati JSON che inviamo con POST/PUT
+app.use('/posts', postsRouter); // Tutte le rotte saranno prefissate con /posts
+
 
 app.listen(PORT,()=>{
     console.log(`Server in ascolto su http://localhost:${PORT}`);
