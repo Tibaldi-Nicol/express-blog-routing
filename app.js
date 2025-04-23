3316b75ad3ed329f11ef18ba3996e106b63737b8
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-04-23 23:54:08 +0200

    [UPD]Server-attivo

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+
+
+
 const express = require('express');
 const app = express();
 const PORT = 3000;
+
+
+app.get('/',(req, res)=>{
+    res.send('Server Express Attivo!')
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server in ascolto su http://localhost:${PORT}`);
+});
+
+
+
