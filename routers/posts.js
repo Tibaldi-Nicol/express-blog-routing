e814a11ad153e2d3f2e76780c0dd02e5119600ae
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-04-24 00:18:33 +0200

    [UPD]Done-post.js

--- routers/posts.js
+++ routers/posts.js
@@ -5,3 +5,28 @@ const router = express.Router();
 router.get('/',(req, res)=>{
     res.send('lista post');
 });
+
+// SHOW - mostra 1 post specifico
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    res.send(`Visualizzazione del post ${id}`);
+  });
+  
+  // CREATE - crea un post
+  router.post('/', (req, res) => {
+    res.send('Creazione di un nuovo post');
+  });
+  
+  // UPDATE - modifica un post esistente
+  router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    res.send(`Aggiornamento del post ${id}`);
+  });
+  
+  // DELETE - elimina un post
+  router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    res.send(`Cancellazione del post ${id}`);
+  });
+  
+  module.exports = router;
